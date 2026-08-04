10fa8c2420358437984ce2db865d0ec1b08c10bf
Mekdi-kassa <mekdelawitkassa6@gmail.com>
2026-08-04 15:55:38 -0700

    Serve uploaded media files through explicit Django route

--- gateguard/urls.py
+++ gateguard/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,6 +14,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL)
+    urlpatterns += [path(settings.STATIC_URL.lstrip("/"), serve, {"document_root": settings.STATIC_ROOT})]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT})]
